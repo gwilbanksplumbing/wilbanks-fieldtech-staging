d3551a311f136a7a9e23a3b4a56d0278a4b95b9a
Glenn Wilbanks <gwillma@gmail.com>
2026-05-29 00:02:51 +0000

    Bump SW cache to FT20 to force new bundle load

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Wilbanks Field Tech Service Worker
 // Cache name: bump on every deployment that changes assets
-const CACHE_NAME = 'wc-fieldtech-v20260527-FT19';
+const CACHE_NAME = 'wc-fieldtech-v20260528-FT20';
 
 const URLS_TO_CACHE = [
   '.',
